d5a7da22e1e59ba21747b74f5eab2527cf6696bd
brada1604 <bagusnugroho1604@gmail.com>
2023-02-23 14:21:11 +0700

    Initial commit

---
+++ database/migrations/2022_12_14_064836_create_detail_transaksi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_transaksi');
+            $table->bigInteger('id_product');
+            $table->float('price_buy', 20, 2, true);
+            $table->bigInteger('qty');
+            $table->float('subtotal', 20, 2, true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksi');
+    }
+};
